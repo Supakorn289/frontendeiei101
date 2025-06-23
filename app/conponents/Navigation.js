bd5d85d38f0336f44b7e9006a0d0cfebff9e14e2
Supakorn289 <supakornprammano@gmail.com>
2025-06-23 12:30:50 +0700

    1

--- app/conponents/Navigation.js
+++ app/conponents/Navigation.js
@@ -6,10 +6,10 @@ export default function Navigation() {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid #eee' }}>
       <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-        <li><Link href="/">หน้าแรก</Link></li>
-        <li><Link href="/about">เกี่ยวกับ</Link></li>
-        <li><Link href="/service">บริการ</Link></li>
-        <li><Link href="/contact">ติดต่อ</Link></li>
+        <li id='home'><Link href="/">หน้าแรก</Link></li>
+        <li id='about'><Link href="/about">เกี่ยวกับ</Link></li>
+        <li id='service'><Link href="/service">บริการ</Link></li>
+        <li id='contact'><Link href="/contact">ติดต่อ</Link></li>
       </ul>
     </nav>
   );
