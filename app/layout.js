09fdb39e428607d28b3ca42f218bd088cda16716
Supakorn289 <supakornprammano@gmail.com>
2025-07-07 09:10:46 +0700

    1

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Prompt } from 'next/font/google';
-import "./globals.css";
 import Navigation from "./components/Navigation";
-import ThemeToggle from './components/ThemeToggle';
 
 
 const prompt = Prompt({
@@ -23,7 +21,6 @@ export default function RootLayout({ children }) {
       <body>
         <div className="navbar">
           <Navigation />
-          <ThemeToggle />
         </div>
         <main style={{ padding: '2rem', textAlign: 'center' }}>
           {children}
