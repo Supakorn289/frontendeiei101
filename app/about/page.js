deb402bdae3a31f7e95385a9f31b0d15cf24c5cc
Supakorn289 <supakornprammano@gmail.com>
2025-08-04 13:31:51 +0700

    1

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,5 @@
+//import Card from "../components/cardabout"  
+
 export default function About() {
   return(
     <>
---
+++ app/components/cardabout.js
@@ -0,0 +1,13 @@
+export default function Card() {
+  return(
+  <div className="card text-bg-dark" width="50px">
+  <img src="https://cdn.readawrite.com/articles/13243/13242343/thumbnail/large.gif?1" className="card-img" alt="..."  width={50} height={50}/>
+  <div className="card-img-overlay">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+  </div>
+</div>
+
+    )
+    }   
